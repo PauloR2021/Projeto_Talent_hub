952aa9dcf72d47cba180d45d92ba00ce8ba3cc6c
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-09-04 18:58:06 -0300

    Add: Criando os Repositorios da aplicação

---
+++ src/main/java/com/prsoftware/talent_hub/repository/CandidaturaRepository.java
@@ -0,0 +1,9 @@
+package com.prsoftware.talent_hub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prsoftware.talent_hub.entity.Candidatura;
+
+public interface CandidaturaRepository extends JpaRepository<Candidatura, Long>{
+
+}
---
+++ src/main/java/com/prsoftware/talent_hub/repository/VagaRepository.java
@@ -0,0 +1,9 @@
+package com.prsoftware.talent_hub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prsoftware.talent_hub.entity.Vaga;
+
+public interface VagaRepository extends JpaRepository <Vaga, Long>{
+
+}
