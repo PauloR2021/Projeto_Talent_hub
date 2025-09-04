5fcc6f167ab01162619f9530c6bd46b2d8a36138
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-09-04 18:53:14 -0300

    Add: Criando a classe de DTO para Vagas

---
+++ src/main/java/com/prsoftware/talent_hub/dto/VagaDTO.java
@@ -0,0 +1,12 @@
+package com.prsoftware.talent_hub.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class VagaDTO {
+    private String titulo;
+    private String descricao;
+    private String requisitos;
+
+}
