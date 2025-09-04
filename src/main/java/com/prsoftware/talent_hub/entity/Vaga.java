dd4cc853e1eafa52684dcafe1062334e700bfef8
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-09-04 18:47:59 -0300

    Add: Adicionando a classe de Vagas

---
+++ src/main/java/com/prsoftware/talent_hub/entity/Vaga.java
@@ -0,0 +1,42 @@
+package com.prsoftware.talent_hub.entity;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "vagas")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+
+public class Vaga {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false) private String titulo;
+    @Column(columnDefinition = "TEXT", nullable = false) private String descricao;
+    @Column(columnDefinition = "TEXT", nullable = false) private String requisitos;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false) private Status status;
+    @Column(name="data_criacao", nullable = false) private LocalDateTime dataCriacao;
+    @OneToMany(mappedBy="vaga", cascade=CascadeType.ALL, orphanRemoval=true)
+    private Set<Candidatura> candidaturas = new HashSet<>();
+    public enum Status { ABERTA, FECHADA }
+
+    @PrePersist
+    public void prePersist() { this.dataCriacao = LocalDateTime.now(); if(status==null) status=Status.ABERTA; }
+}
